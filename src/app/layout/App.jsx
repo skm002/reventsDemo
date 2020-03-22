bb2b61102e1b01a17103ca4feb94e2c0f1624ae5
Sravanth KM <sravanth.k.m@pwc.com>
2020-03-22 23:57:51 +0530

    End of section 3

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,11 +1,18 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
+import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
+import NavBar from '../../features/nav/NavBar/NavBar'
+import { Container } from 'semantic-ui-react';
+import EventList from '../../features/event/EventList/EventList';
 
 class App extends Component {
   render(){
     return (
-      <div>
-        <h1> Re-vents </h1>
-      </div>
+      <Fragment>
+        <NavBar />
+        <Container className="main">
+          <EventDashboard />
+        </Container>
+      </Fragment>
     );
   }
 }
---
+++ src/features/event/EventList/EventListAttendee.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { Image, List } from 'semantic-ui-react';
+
+class EventListAttendee extends Component {
+    render() {
+        const {attendee} = this.props;
+        return (
+            
+            <div>
+                <List.Item>
+                    <Image as='a' size='mini' circular src= {attendee.photoURL} />
+                </List.Item>
+            </div>
+        )
+    }
+}
+
+export default EventListAttendee;
